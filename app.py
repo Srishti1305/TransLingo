0c70c7f3fe7a43fe25b5ad70a73734368320dac5
Srishti Yadav <srishtiy1301@outlook.com>
2025-07-23 12:00:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,6 +23,6 @@ def translate():
 
 if __name__ == '__main__':
       port = int(os.environ.get("PORT", 5000))
-      app.run(host='0.0.0.0', port=port, debug=True)
+      app.run(host='0.0.0.0', port=port, debug=False)
 
 Bind Flask to 0.0.0.0 using PORT env var for Render
